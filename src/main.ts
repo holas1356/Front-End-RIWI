3fa8b5a067263efbe598b6af78935b7132c6650b
holas1356 <ruthbustamante075@gmail.com>
2024-08-06 15:24:15 -0500

    The header is created with the profile photo

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import './assets/main.css'
-
+import './assets/main.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
+
+
+import '@fortawesome/fontawesome-free/css/all.css'
 
 createApp(App).mount('#app')
